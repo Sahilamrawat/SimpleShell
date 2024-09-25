fa1f3d2cacf84d39b41a0784b6341a7fa7c8bb0e
Sahilamr <162734489+Sahilamr@users.noreply.github.com>
2024-09-25 23:39:42 +0530

    Update fib.c

--- my_shell/fib.c
+++ my_shell/fib.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /*
  * No changes are allowed in this file
  */
 int fib(int n) {
-  if(n<2) return n;
-  else return fib(n-1)+fib(n-2);
+  if(n < 2) return n;
+  else return fib(n-1) + fib(n-2);
 }
-int main() {
-    int n;
-    scanf("%d",&n);
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Please provide a number as a command-line argument.\n");
+        return 1;
+    }
+
+    int n = atoi(argv[1]);  // Convert the first argument to an integer
+
     int val = fib(n);
-    printf("Answer is '%d'\n",val);
+    printf("Answer is '%d'\n", val);
+
+    return 0;
 }
